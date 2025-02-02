479904cd30c45aa1fc188cdc3a81bae7b577d301
m4st3 <runtycybin@gmail.com>
2025-02-02 18:10:15 +0100

    initial commit

---
+++ src/test/java/com/carry/basar/CarrybasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carry.basar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrybasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
