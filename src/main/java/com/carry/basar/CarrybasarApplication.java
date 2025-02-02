63590ade85a958e586b32eb4403900748cbfd369
m4st3 <runtycybin@gmail.com>
2025-02-02 18:10:26 +0100

    initial commit

---
+++ src/main/java/com/carry/basar/CarrybasarApplication.java
@@ -0,0 +1,13 @@
+package com.carry.basar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrybasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrybasarApplication.class, args);
+	}
+
+}
