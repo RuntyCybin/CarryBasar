673994500b610e3a81edd546cca8d7a806f1bf8c
m4st3 <runtycybin@gmail.com>
2025-02-27 20:00:15 +0100

    [add_roles_management]: big user service refactor

--- src/main/java/com/carry/basar/service/UserService.java
+++ src/main/java/com/carry/basar/service/UserService.java
@@ -2,9 +2,14 @@ package com.carry.basar.service;
 
 import com.carry.basar.model.User;
 import com.carry.basar.config.JwtUtil;
+import com.carry.basar.model.UserRol;
+import com.carry.basar.model.dto.user.CreateUserRequest;
+import com.carry.basar.model.repository.RoleRepository;
 import com.carry.basar.model.repository.UserRepository;
+import com.carry.basar.model.repository.UserRolRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -12,11 +17,15 @@ public class UserService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    private final UserRolRepository userRolRepository;
 
-    public UserService(UserRepository userRepository, JwtUtil jwtUtil, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, JwtUtil jwtUtil, PasswordEncoder passwordEncoder, RoleRepository roleRepository, UserRolRepository userRolRepository) {
         this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
         this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+        this.userRolRepository = userRolRepository;
     }
 
     public Mono<String> authenticate(String username, String password) {
@@ -31,11 +40,47 @@ public class UserService {
                 });
     }
 
-    public Mono<User> register(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user).onErrorResume(e -> {
-            System.out.println("Error saving a user: " + e.getMessage());
-            return Mono.error(new RuntimeException("Error saving a user: ", e));
-        });
+    public Mono<User> register(CreateUserRequest createUserRequest) {
+        // mapping CreateUserRequest to User
+        User user = mapUser(createUserRequest);
+        return userRepository.save(user)
+                .flatMap(savedUser -> {
+                    if (savedUser.getId() == null) {
+                        return Mono.error(new RuntimeException("El ID del usuario guardado es null"));
+                    }
+                    System.out.println("User saved: " + savedUser.getId());
+
+                    // 2. Buscar roles de forma reactiva
+                    return Flux.fromIterable(createUserRequest.getRoles())
+                            .flatMap(roleName ->
+                                    roleRepository.findByName(roleName)
+                                            .flatMap(role -> {
+                                                if (role == null || role.getRolId() == null) {
+                                                    return Mono.error(new RuntimeException("Rol no encontrado: " + roleName));
+                                                }
+                                                UserRol userRol = new UserRol(null, savedUser.getId(), role.getRolId());
+                                                System.out.println("Creando UserRol: " + userRol.getUserId());
+                                                return Mono.just(userRol);
+                                            })
+                            )
+                            .doOnNext(userRol -> System.out.println("UserRol listo para ser guardado: " + userRol))
+                            .collectList()
+                            .flatMapMany(userRolRepository::saveAll)
+                            .doOnNext(userRol -> System.out.println("Insertando en usuarios_roles: " + userRol))
+                            .collectList()
+                            .thenReturn(savedUser);
+                })
+                .onErrorResume(e -> {
+                    System.out.println("Error saving a user: " + e.getMessage());
+                    return Mono.error(new RuntimeException("Error saving a user: ", e));
+                });
+    }
+
+    private User mapUser(CreateUserRequest createUserRequest) {
+        User user = new User();
+        user.setName(createUserRequest.getUsername());
+        user.setEmail(createUserRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
+        return user;
     }
 }
