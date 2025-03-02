e718c0e9deab4a759ea4652ed138d3ff5602f6f9
Nikolay Shelevoy <nikolaysh@ext.inditex.com>
2025-03-02 17:17:39 +0100

    [14-add-validation]: scripts.sql, gitignore and role repo updated

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -6,23 +6,32 @@ CREATE SCHEMA transportsschema;
 
 -- 1.
 CREATE TABLE transportsschema.users (
-    id serial PRIMARY KEY,
-    name VARCHAR(100),
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) not null,
     email VARCHAR(100) UNIQUE not null,
     password VARCHAR(100) not null
 );
 
 -- 2.
 CREATE TABLE transportsschema.roles (
-  id serial PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   descripcion varchar(100)
 );
 
 -- 3.
 CREATE TABLE transportsschema.usuarios_roles (
-  id serial PRIMARY KEY,
-  usuario serial,
-  rol serial
+  id SERIAL PRIMARY KEY,
+  usuario SERIAL,
+  rol SERIAL
+);
+
+-- 4.
+CREATE TABLE transportsschema.orders (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(200),
+    user_id int references transportsschema.users(id),
+    vol int not null,
+    order_date timestamp default current_timestamp
 );
 
 
@@ -30,12 +39,10 @@ ALTER TABLE transportsschema.usuarios_roles ADD FOREIGN KEY ("usuario") REFERENC
 
 ALTER TABLE transportsschema.usuarios_roles ADD FOREIGN KEY ("rol") REFERENCES transportsschema.roles (id);
 
-CREATE SEQUENCE cybinschema.users_seq;
-
-
+CREATE SEQUENCE transportsschema.users_seq;
 
 -- actualiza la secuencia para que comience en 4
-ALTER SEQUENCE cybinschema.users_seq RESTART WITH 1;
+ALTER SEQUENCE transportsschema.users_seq RESTART WITH 1;
 
 
 -- Inserting sample roles
