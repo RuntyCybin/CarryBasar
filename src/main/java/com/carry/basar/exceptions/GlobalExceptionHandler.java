527fbc8c37c9a83c5dc0d4f4f7ab515f8245b644
Nikolay Shelevoy <nikolaysh@ext.inditex.com>
2025-03-08 20:09:19 +0100

    [10-implement-an-exception-handling]: GlobalExceptionHandler created

---
+++ src/main/java/com/carry/basar/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.carry.basar.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getReason());
+        response.put("status", ex.getStatusCode().toString());
+        response.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        return response;
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleRuntimeExceptions(RuntimeException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error: ", ex.getMessage());
+        response.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        return response;
+    }
+}
