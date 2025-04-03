1643c3f8aa21f342dd0e44209e66ea37c9b37820
Nikolay Shelevoy <runtycybin@gmail.com>
2025-04-03 10:49:31 +0200

    [13-crear-un-endpoint-para-actualizar-los-roles-de-un-usuario]: update user request created

---
+++ src/main/java/com/carry/basar/model/dto/role/UpdateRoleRequest.java
@@ -0,0 +1,14 @@
+package com.carry.basar.model.dto.role;
+
+public class UpdateRoleRequest {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
