2ec6ae1858a598d0bec74b0edd3d6cb79f79d438
Nikolay Shelevoy <runtycybin@gmail.com>
2025-04-03 10:58:50 +0200

    [13-crear-un-endpoint-para-actualizar-los-roles-de-un-usuario]: a response dto made for users updates

---
+++ src/main/java/com/carry/basar/model/dto/user/UpdateUserResponse.java
@@ -0,0 +1,28 @@
+package com.carry.basar.model.dto.user;
+
+public class UpdateUserResponse {
+  private String username;
+  private String email;
+  private String role;
+
+  public UpdateUserResponse() {
+  }
+
+  public UpdateUserResponse(String username, String email, String role) {
+    this.username = username;
+    this.email = email;
+    this.role = role;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getRole() {
+    return role;
+  }
+}
