4067c1cefa4f713e085f7e7e9b7c58d8c8b10559
Nikolay Shelevoy <runtycybin@gmail.com>
2025-05-11 22:36:36 +0200

    [6-move-the-front]: frontend dashboard updated

--- src/main/resources/static/public/js/dashboard.js
+++ src/main/resources/static/public/js/dashboard.js
@@ -19,9 +19,33 @@
             return res.json();
         })
         .then(data => {
-            console.log("LLEGO2");
             console.log("RESPONSE: " + JSON.stringify(data, null, 2));
-            //document.getElementById('datos').textContent = JSON.stringify(data, null, 2);
+            const listContainer = document.getElementById("ordersList");
+            listContainer.innerHTML = ""; // Limpiar contenido previo
+            // iteramos el data
+            for (let i = 0; i < data.length; i++) {
+                const order = data[i];
+                console.log(`Pedido ${i + 1}: ${order.description}, volumen ${order.volume}, creado el ${order.createdAt}`);
+
+                const item = document.createElement("a");
+                item.href = "#";
+                item.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
+
+                item.innerHTML = `
+                    <img src="https://github.com/twbs.png" alt="" width="32" height="32"
+                        class="rounded-circle flex-shrink-0">
+                    <div class="d-flex gap-2 w-100 justify-content-between">
+                    <div>
+                        <h6 class="mb-0">Pedido ${i + 1}</h6>
+                        <p class="mb-0 opacity-75">${order.description}</p>
+                        <p class="mb-0 opacity-75">Volumen: ${order.volume}</p>
+                    </div>
+                    <small class="opacity-50 text-nowrap">${new Date(order.createdAt).toLocaleString()}</small>
+                    </div>
+                `;
+
+                listContainer.appendChild(item);
+            }
         })
         .catch(err => {
             console.error(err);
