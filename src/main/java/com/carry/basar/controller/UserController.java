08734960515f945a98357922b64f60d6bd96bc74
Nikolay Shelevoy <runtycybin@gmail.com>
2025-05-11 22:38:27 +0200

    [6-move-the-front]: check user token endpoint

--- src/main/java/com/carry/basar/controller/UserController.java
+++ src/main/java/com/carry/basar/controller/UserController.java
@@ -9,6 +9,8 @@ import com.carry.basar.service.UserService;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -31,6 +33,15 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<User> register(@Valid @RequestBody CreateUserRequest user) {
         return service.register(user);
+    }
+
+    @GetMapping("/check")
+    public Mono<ResponseEntity<Void>> checkIfTokenIsValid() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(ctx -> ctx.getAuthentication())
+                .filter(auth -> auth != null && auth.isAuthenticated())
+                .map(auth -> ResponseEntity.ok().<Void>build())
+                .defaultIfEmpty(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
     
 }
