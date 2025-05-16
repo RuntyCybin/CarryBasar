232d3bdb11fce8de9f5d65c7cb6863faed42fdbe
Nikolay Shelevoy <runtycybin@gmail.com>
2025-05-17 01:04:02 +0200

    [32-back-end-get-accepted-orders-for-carry-role]: dashboard refactor to print the orders of the carry user

--- src/main/resources/static/public/js/dashboard.js
+++ src/main/resources/static/public/js/dashboard.js
@@ -16,6 +16,76 @@
         console.log("ROL USER and TRANSPORTER");
 
         // mostramos orders que se pueden llevar
+        fetch('/v1/api/order/all', {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then(async res => {
+
+                // Gestión del 500
+                if (res.status == 500) {
+                    // Leemos el cuerpo para obtener el mensaje que envía el backend
+                    const errorBody = await res.json().catch(() => ({}));
+                    const errorMsg = errorBody.error || "Internal server error";
+                    throw new Error(errorMsg);
+                }
+
+                // Gestión del 401/403 (no autorizado)
+                if (!res.ok) throw new Error('No autorizado');
+
+                // Todo correcto → devolvemos JSON
+                return res.json();
+
+            })
+            .then(data => {
+                console.log("RESPONSE: " + JSON.stringify(data, null, 2));
+                const listContainer = document.getElementById("ordersList");
+                listContainer.innerHTML = ""; // Limpiar contenido previo
+                // iteramos el data
+                for (let i = 0; i < data.length; i++) {
+                    const order = data[i];
+                    console.log(`Pedido ${i + 1}: ${order.description}, volumen ${order.volume}, creado el ${order.createdAt}`);
+
+                    const item = document.createElement("a");
+                    item.href = "#";
+                    item.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
+
+                    item.innerHTML = `
+                    <img src="https://github.com/twbs.png" alt="" width="32" height="32"
+                        class="rounded-circle flex-shrink-0">
+                    <div class="d-flex gap-2 w-100 justify-content-between">
+                    <div>
+                        <h6 class="mb-0">Pedido ${i + 1}</h6>
+                        <p class="mb-0 opacity-75">${order.description}</p>
+                        <p class="mb-0 opacity-75">Volumen: ${order.volume}</p>
+                    </div>
+                    <small class="opacity-50 text-nowrap">${new Date(order.createdAt).toLocaleString()}</small>
+                    </div>
+                `;
+
+                    listContainer.appendChild(item);
+                }
+            })
+            .catch(err => {
+                console.error(err);
+
+                // Si fue un 500 mostramos el mensaje del backend, si no, mensaje genérico
+                alert(err.errorMsg.includes("Internal server")
+                    ? 'No se pudieron obtener tus pedidos: ' + err.message
+                    : 'Sesión inválida. Inicie sesión nuevamente.');
+
+                sessionStorage.clear();
+                window.location.href = '/public/login.html';
+            });
+
+    } else if (roles.includes("USER") && roles.includes("CARRY")) {
+        console.log("ROL USER and CARRY");
+
+        const descripcionDashboard = document.getElementById("descDashboard");
+        descripcionDashboard.innerHTML = `<p id="descDashboard">Listado de orders creados por <span>${username}</span>.</p>`;
+
+        // mostramos orders que tiene el usuario creados
         fetch('/v1/api/order/my-orders', {
             headers: {
                 'Authorization': 'Bearer ' + token
@@ -69,19 +139,15 @@
             })
             .catch(err => {
                 console.error(err);
-                
+
                 // Si fue un 500 mostramos el mensaje del backend, si no, mensaje genérico
-                alert(err.errorMsg.includes("Internal server") 
-                ? 'No se pudieron obtener tus pedidos: ' + err.message 
-                : 'Sesión inválida. Inicie sesión nuevamente.');
-                
+                alert(err.errorMsg.includes("Internal server")
+                    ? 'No se pudieron obtener tus pedidos: ' + err.message
+                    : 'Sesión inválida. Inicie sesión nuevamente.');
+
                 sessionStorage.clear();
                 window.location.href = '/public/login.html';
             });
-
-    } else if (roles.includes("USER") && !roles.includes("CARRY")) {
-        console.log("ROL USER and CARRY");
-        // mostramos orders que tiene el usuario creados
     } else if (roles.includes("ADMIN")) {
         window.location.href = '/public/dashboardAdmin.html';
         return;
