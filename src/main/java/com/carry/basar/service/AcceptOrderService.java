c6fe3770a2eb95978438b75461d8eb13283ae3fe
Nikolay Shelevoy <runtycybin@gmail.com>
2025-05-30 12:40:33 +0200

    [40-back-end-accept-an-order]: accepted order service

---
+++ src/main/java/com/carry/basar/service/AcceptOrderService.java
@@ -0,0 +1,9 @@
+package com.carry.basar.service;
+
+import com.carry.basar.model.AcceptedOrders;
+import com.carry.basar.model.dto.accepted_order.AcceptOrderRequest;
+import reactor.core.publisher.Mono;
+
+public interface AcceptOrderService {
+  Mono<AcceptedOrders> createOrder(AcceptOrderRequest request);
+}
