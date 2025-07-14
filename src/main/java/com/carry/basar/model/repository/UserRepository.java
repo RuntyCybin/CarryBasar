3e2452dd26169a7b8004754a84783697c5fce21d
Nikolay Shelevoy <runtycybin@gmail.com>
2025-07-14 11:32:34 +0200

    [23-back-end-list-user-roles]: list user roles by user name rdbc repo created

--- src/main/java/com/carry/basar/model/repository/UserRepository.java
+++ src/main/java/com/carry/basar/model/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.carry.basar.model.repository;
 
 import com.carry.basar.model.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
     Mono<User> findByName(String name);
     Mono<User> findByEmail(String email);
+    Flux<String> findRolesByUserName(String userName);
 }
