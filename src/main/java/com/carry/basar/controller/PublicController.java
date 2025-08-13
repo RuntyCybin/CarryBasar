cf540c3e879e2696147e232f6b31bd182046adb4
Nikolay Shelevoy <runtycybin@gmail.com>
2025-08-13 20:28:03 +0200

    [43-back-end-remove-an-accepted-order-endpoint]: User controller renamed to public controller and refactor

--- src/main/java/com/carry/basar/controller/PublicController.java
+++ src/main/java/com/carry/basar/controller/PublicController.java
@@ -3,6 +3,7 @@ package com.carry.basar.controller;
 import com.carry.basar.model.dto.auth.AuthRequest;
 import com.carry.basar.model.User;
 import com.carry.basar.model.dto.auth.AuthResponse;
+import com.carry.basar.model.dto.role.RolesListResponse;
 import com.carry.basar.model.dto.user.CreateUserRequest;
 import com.carry.basar.model.dto.user.RecoverPwdRequest;
 import com.carry.basar.model.dto.user.RecoverPwdResponse;
@@ -14,15 +15,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/public/usr")
-public class UserController {
+public class PublicController {
 
     private final UserService service;
 
-    public UserController(UserService service) {
+    public PublicController(UserService service) {
         this.service = service;
     }
 
@@ -31,9 +33,15 @@ public class UserController {
         return service.authenticate(authRequest.getUsername(), authRequest.getPassword());
     }
 
+    @GetMapping("/getPublicRoles")
+    public Flux<RolesListResponse> listPublicRoles() {
+        return service.listPublicRoles();
+    }
+
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<User> register(@Valid @RequestBody CreateUserRequest user) {
+        System.out.println("CONTROLLER USER: " + user.getUsername());
         return service.register(user);
     }
 
