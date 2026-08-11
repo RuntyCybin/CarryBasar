5962a731551527632086bc86a48f2d326f49401e
Nikolay Shelevoy <runtycybin@gmail.com>
2026-08-12 00:01:39 +0300

    [bugfix/develop]: role dto refactor

--- src/main/java/com/carry/basar/model/dto/RoleDto.java
+++ src/main/java/com/carry/basar/model/dto/RoleDto.java
@@ -1,20 +1,20 @@
 package com.carry.basar.model.dto;
 
 public class RoleDto {
-    private String name;
+  private String name;
 
-    public RoleDto() {
-    }
+  public RoleDto() {
+  }
 
-    public RoleDto(String name) {
-        this.name = name;
-    }
+  public RoleDto(String name) {
+    this.name = name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 }
