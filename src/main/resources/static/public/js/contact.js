812e2323de6a324f27aa17b08c002be0ac4c7e17
Nikolay Shelevoy <runtycybin@gmail.com>
2026-08-16 23:52:59 +0300

    [feature/60-substitute-bootstrap-with-tailwind]: switch from bootstrap to tailwind

---
+++ src/main/resources/static/public/js/contact.js
@@ -0,0 +1,38 @@
+(function () {
+    const token = sessionStorage.getItem('token');
+
+    // comprobamos si el token existe
+    if (!token) {
+        window.location.href = '/public/login.html';
+        return;
+    }
+
+    const username = sessionStorage.getItem('username');
+    const roles = sessionStorage.getItem('roles');
+
+    document.getElementById('username').textContent = username;
+
+    // comprobamos el rol de usuario
+    if (roles.includes("USER") && roles.includes("TRANSPORTER")) {
+        // indicador del rol en la barra de navegacion
+        const role_nav_span = document.getElementById("roleUser");
+        role_nav_span.textContent = "[TRANSPORTISTA]";
+
+    } else if (roles.includes("USER") && roles.includes("CARRY")) {
+
+    } else if (roles.includes("ADMIN")) {
+        window.location.href = '/public/dashboardAdmin.html';
+        return;
+    }
+
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        console.log('Formulario enviado:', { name, email, message });
+        alert('¡Gracias por contactarnos!');
+        this.reset();
+    });
+})();
